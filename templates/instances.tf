94696fdcea61567955dc0884c558d61f10019e9f
Andreas Trawoeger <atrawog@dorgeln.org>
2021-12-07 14:08:59 +0000

    Add terraform apply playbook.

--- templates/instances.tf
+++ templates/instances.tf
@@ -1,14 +1,13 @@
-resource "openstack_compute_instance_v2" "tf1" {
+resource "openstack_compute_instance_v2" "tf1-{{ TF_PROVIDER }}" {
    name        = "tf1-{{ TF_PROVIDER }}" # Instance name
    provider    = openstack.ovh  # Provider name
    image_name  = "Ubuntu 20.04" # Image name
    flavor_name = "s1-2" # Instance type name
    metadata = {
-     ansible_group = "terrabuilder"
+     ansible_group = "infrabuilder"
      ansible_user = "ubuntu"
    }
-   # Name of openstack_compute_keypair_v2 resource named keypair_test
-   key_pair    = openstack_compute_keypair_v2.terrabuilder.name
+   key_pair    = openstack_compute_keypair_v2.infrabuilder-{{ TF_PROVIDER }}.name
    network {
      name      = "Ext-Net" # Adds the network component to reach your instance
    }
--- templates/ssh.tf
+++ templates/ssh.tf
@@ -1,5 +1,5 @@
-resource "openstack_compute_keypair_v2" "terrabuilder" {
+resource "openstack_compute_keypair_v2" "infrabuilder-{{ TF_PROVIDER }}" {
   provider   = openstack.ovh # Provider name declared in provider.tf
-  name       = "infrabuilder" # Name of the SSH key to use for creation
+  name       = "infrabuilder-{{ TF_PROVIDER }}" # Name of the SSH key to use for creation
   public_key = file("id_rsa.pub") # Path to your previously generated SSH key
 }
