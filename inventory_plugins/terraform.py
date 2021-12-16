fb2600c1fd03ecb7f062d9a9d0446580b4de7f28
Andreas Trawoeger <atrawog@dorgeln.org>
2021-12-16 14:14:40 +0000

    Debug terraform state

--- inventory_plugins/terraform.py
+++ inventory_plugins/terraform.py
@@ -50,10 +50,8 @@ class InventoryModule(BaseInventoryPlugin):
             raise AnsibleParserError(
                 'All correct options required: {}'.format(e))
         
-        tfstate_run = subprocess.run(['terraform','state','pull','>','tfstate.json'],cwd=self.project_path, capture_output=True)
+        tfstate_run = subprocess.run(['terraform','state','pull','>','tfstate.json'],cwd=self.project_path, capture_output=False)
 
-        print("---\nDEBUG terraform state stdout")
-        print(tfstate_run.stdout)
         print("---\nDEBUG terraform state stderr")
         print(tfstate_run.stderr)
         tfstate=json.loads(tfstate_run.stdout)
