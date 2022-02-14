983e5bbcba8492274559b8806b099b4a47182321
Andreas Trawoeger <atrawog@gmail.com>
2022-02-14 15:13:45 +0100

    Save certbot script in state repo.

--- roles/certbot/templates/certbot.sh
+++ roles/certbot/templates/certbot.sh
@@ -1,10 +1,10 @@
-../.micromamba/envs/default/bin/certbot certonly --force-renewal {% if staging %}--test-cert{% endif %} \
+../../../../.micromamba/envs/default/bin/certbot certonly --force-renewal {% if staging %}--test-cert{% endif %} \
     --dns-ovh --non-interactive --agree-tos \
-    -m {{email}} --dns-ovh-credentials ./ovh.ini \
+    -m {{email}} --dns-ovh-credentials ../../../../.secrets/ovh.ini \
     --cert-name {{fqdn}} -d {{fqdn}} \
     {% for d in aliases %}-d {{d}} {% endfor %} \
     {% for d in fqdns %}-d {{d}} {% endfor %} \
     {% for h in hostnames %}-d {{h}}.{{domain}} {% endfor %} \
-    --work-dir ../{{certs}} --config-dir ../{{certs}} \
-    --logs-dir ../{{certs}} --dns-ovh-propagation-seconds 60 
+    --work-dir . --config-dir . \
+    --logs-dir . --dns-ovh-propagation-seconds 60 
 
